7db08e407f7152b09e2823a2d81bd4bf5e1c8d10
duyhelloworld <codedaovoiduy@gmail.com>
2023-03-04 11:17:09 +0700

    Discord buổi 1: đọc MVC,  sợ cách dùng 1 framework

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -19,6 +19,7 @@ public class WeatherForecastController : ControllerBase
     }
 
     [HttpGet(Name = "GetWeatherForecast")]
+    // []
     public IEnumerable<WeatherForecast> Get()
     {
         return Enumerable.Range(1, 5).Select(index => new WeatherForecast
--- README.md
+++ README.md
@@ -1,5 +1,8 @@
 # Welcome to our essay about .NET framework
 
+<!-- Get Template -->
+`dotnet new webapi -f net7.0 -n essay-se-dotnetfw --no-https`
+
 1. Ctrl + `
 2. dotnet build
 3. dotnet run (dừng bằng Ctrl C)
